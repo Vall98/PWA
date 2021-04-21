5daee8d0508e46c62ed76240bc36513a5eaa952b
Alexis CRAYE <alexis.craye@rideyego.com>
2021-04-21 21:34:39 +0200

    Updating heroku deployment

--- my-pwa/release-tasks.sh
+++ my-pwa/release-tasks.sh
@@ -5,3 +5,15 @@ export const environment = {
   api_token: "$BACKEND_API_TOKEN"
 }
 EOF
+cat > src/firebase-config.js <<EOF
+const firebaseConfig = {
+  apiKey: "$FIREBASE_API_TOKEN",
+  authDomain: "test-pushnotif-58b2d.firebaseapp.com",
+  databaseURL: "https://test-pushnotif-58b2d.firebaseio.com/",
+  projectId: "test-pushnotif-58b2d",
+  storageBucket: "test-pushnotif-58b2d.appspot.com",
+  messagingSenderId: "734454521373",
+  appId: "1:734454521373:web:50a48b2d47a0c59206f0f4"
+};
+EOF
+exit 0
