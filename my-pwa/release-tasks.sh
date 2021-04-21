8836aeca539c47c9df5f8d3895dfb8f1d5aa63b5
Alexis CRAYE <alexis.craye@rideyego.com>
2021-04-21 22:25:13 +0200

    update deploy script

--- my-pwa/release-tasks.sh
+++ my-pwa/release-tasks.sh
@@ -4,14 +4,32 @@ cat > src/environments/environment.prod.ts <<EOF
 export const environment = {
   api: "https://free-sons-backend.herokuapp.com/",
   production: true,
-  api_token: "$BACKEND_API_TOKEN"
+  api_token: "$BACKEND_API_TOKEN",
+  firebaseConfig: {
+    apiKey: "$FIREBASE_API_TOKEN",
+    authDomain: "test-pushnotif-58b2d.firebaseapp.com",
+    databaseURL: "https://test-pushnotif-58b2d.firebaseio.com/",
+    projectId: "test-pushnotif-58b2d",
+    storageBucket: "test-pushnotif-58b2d.appspot.com",
+    messagingSenderId: "734454521373",
+    appId: "1:734454521373:web:50a48b2d47a0c59206f0f4"
+  }
 }
 EOF
 cat > src/environments/environment.ts <<EOF
 export const environment = {
   api: "https://free-sons-backend.herokuapp.com/",
   production: true,
-  api_token: "$BACKEND_API_TOKEN"
+  api_token: "$BACKEND_API_TOKEN",
+  firebaseConfig: {
+    apiKey: "$FIREBASE_API_TOKEN",
+    authDomain: "test-pushnotif-58b2d.firebaseapp.com",
+    databaseURL: "https://test-pushnotif-58b2d.firebaseio.com/",
+    projectId: "test-pushnotif-58b2d",
+    storageBucket: "test-pushnotif-58b2d.appspot.com",
+    messagingSenderId: "734454521373",
+    appId: "1:734454521373:web:50a48b2d47a0c59206f0f4"
+  }
 }
 EOF
 cat > src/firebase-config.js <<EOF
